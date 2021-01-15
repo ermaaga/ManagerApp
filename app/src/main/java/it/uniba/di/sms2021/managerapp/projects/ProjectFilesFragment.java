d1628e41f0ac44bd7a358c08561776672e1382f2
UnseenPaper <gargiulomario@ymail.com>
2021-01-15 11:10:33 +0100

    Implementata visualizzazione di immagini

--- app/src/main/java/it/uniba/di/sms2021/managerapp/projects/ProjectFilesFragment.java
+++ app/src/main/java/it/uniba/di/sms2021/managerapp/projects/ProjectFilesFragment.java
@@ -7,6 +7,7 @@ import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.core.content.FileProvider;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.DividerItemDecoration;
 import androidx.recyclerview.widget.LinearLayoutManager;
@@ -22,6 +23,7 @@ import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.firebase.storage.FileDownloadTask;
 import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.ListResult;
 import com.google.firebase.storage.OnPausedListener;
@@ -30,16 +32,16 @@ import com.google.firebase.storage.StorageMetadata;
 import com.google.firebase.storage.StorageReference;
 import com.google.firebase.storage.UploadTask;
 
+import java.io.File;
 import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
+import it.uniba.di.sms2021.managerapp.BuildConfig;
 import it.uniba.di.sms2021.managerapp.R;
 import it.uniba.di.sms2021.managerapp.enitities.ManagerFile;
-import it.uniba.di.sms2021.managerapp.enitities.StudyCase;
 import it.uniba.di.sms2021.managerapp.lists.FilesRecyclerAdapter;
-import it.uniba.di.sms2021.managerapp.lists.StudyCasesRecyclerAdapter;
 import it.uniba.di.sms2021.managerapp.utility.FileUtil;
 
 import static android.app.Activity.RESULT_OK;
@@ -48,8 +50,12 @@ public class ProjectFilesFragment extends Fragment implements View.OnClickListen
 
     private static final String TAG = "ProjectFilesFragment";
 
+    // Si possono visualizzare file grandi massimo 10MB
+    private static final int MAX_TEMP_FILE_SIZE = 1024*1024*10;
+    private String gruppo = "Gruppo1";
+
     //TODO rendere riferimento dinamico
-    private StorageReference storageRef = FirebaseStorage.getInstance().getReference().child("Gruppo1");
+    private StorageReference storageRef = FirebaseStorage.getInstance().getReference().child(gruppo);
     private Set<StorageReference> elaboratingReferences;
     private List<ManagerFile> files;
 
@@ -72,8 +78,9 @@ public class ProjectFilesFragment extends Fragment implements View.OnClickListen
         adapter = new FilesRecyclerAdapter(getContext(), new FilesRecyclerAdapter.OnActionListener() {
             @Override
             public void onClick(ManagerFile file) {
-                Toast.makeText(getContext(), R.string.text_message_not_yet_implemented,
-                        Toast.LENGTH_SHORT).show();
+                showFile(file);
+                /*Toast.makeText(getContext(), R.string.text_message_not_yet_implemented,
+                        Toast.LENGTH_SHORT).show();*/
             }
         });
         filesRecyclerView.setAdapter(adapter);
@@ -178,7 +185,7 @@ public class ProjectFilesFragment extends Fragment implements View.OnClickListen
 
     private void selectImage() {
         Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
-        intent.setType("image/*");
+        intent.setType("*/*");
         startActivityForResult(intent, REQUEST_IMAGE_GET);
     }
 
@@ -210,13 +217,13 @@ public class ProjectFilesFragment extends Fragment implements View.OnClickListen
         // Listen for state changes, errors, and completion of the upload.
         uploadTask.addOnProgressListener(new OnProgressListener<UploadTask.TaskSnapshot>() {
             @Override
-            public void onProgress(UploadTask.TaskSnapshot taskSnapshot) {
+            public void onProgress(@NonNull UploadTask.TaskSnapshot taskSnapshot) {
                 double progress = (100.0 * taskSnapshot.getBytesTransferred()) / taskSnapshot.getTotalByteCount();
                 Log.d(TAG, "Upload is " + progress + "% done");
             }
         }).addOnPausedListener(new OnPausedListener<UploadTask.TaskSnapshot>() {
             @Override
-            public void onPaused(UploadTask.TaskSnapshot taskSnapshot) {
+            public void onPaused(@NonNull UploadTask.TaskSnapshot taskSnapshot) {
                 Log.d(TAG, "Upload is paused");
             }
         }).addOnFailureListener(new OnFailureListener() {
@@ -232,5 +239,41 @@ public class ProjectFilesFragment extends Fragment implements View.OnClickListen
                 // ...
             }
         });
+    }
+
+    private void showFile (ManagerFile file) {
+        if (file.getSize() > MAX_TEMP_FILE_SIZE) {
+            Toast.makeText(getContext(), R.string.text_message_temp_file_too_big,
+                    Toast.LENGTH_LONG).show();
+            return;
+        }
+        File path = new File(getContext().getFilesDir(), gruppo);
+        if (!path.exists()) {
+            path.mkdirs();
+        }
+        File localFile = new File(path, file.getName());
+
+        file.getReference().getFile(localFile).addOnSuccessListener(new OnSuccessListener<FileDownloadTask.TaskSnapshot>() {
+            @Override
+            public void onSuccess(FileDownloadTask.TaskSnapshot taskSnapshot) {
+                Uri uri = FileProvider.getUriForFile(getContext(),
+                        BuildConfig.APPLICATION_ID + ".provider", localFile);
+                openFile(uri, file.getType());
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception exception) {
+                Toast.makeText(getContext(), R.string.text_message_preview_open_failed,
+                        Toast.LENGTH_LONG).show();
+            }
+        });
+    }
+
+    private void openFile (Uri uri, String mimeType) {
+        Intent intent = new Intent();
+        intent.setAction(Intent.ACTION_VIEW);
+        intent.setDataAndType(uri, mimeType);
+        intent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
+        startActivity(intent);
     }
 }
