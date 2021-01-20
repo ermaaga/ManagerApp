78b35f56a0e0dca35d25f24d6ebca8ceca0c2ef0
Carecci Sara <s.carecci@studenti.uniba.it>
2021-01-20 13:51:01 +0100

    Implementata versione preliminare caricamento file in firebase per nuovo caso di studio

--- app/src/main/java/it/uniba/di/sms2021/managerapp/exams/NewStudyCaseActivity.java
+++ app/src/main/java/it/uniba/di/sms2021/managerapp/exams/NewStudyCaseActivity.java
@@ -1,26 +1,53 @@
 package it.uniba.di.sms2021.managerapp.exams;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.material.textfield.TextInputEditText;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.OnPausedListener;
+import com.google.firebase.storage.OnProgressListener;
+import com.google.firebase.storage.StorageMetadata;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
 
 import it.uniba.di.sms2021.managerapp.R;
 import it.uniba.di.sms2021.managerapp.db.FirebaseDbHelper;
 import it.uniba.di.sms2021.managerapp.enitities.Exam;
 import it.uniba.di.sms2021.managerapp.enitities.StudyCase;
 import it.uniba.di.sms2021.managerapp.utility.AbstractFormActivity;
+import it.uniba.di.sms2021.managerapp.utility.FileUtil;
 
 //TODO renderlo un fragment?
-public class NewStudyCaseActivity extends AbstractFormActivity {
+public class NewStudyCaseActivity extends AbstractFormActivity implements View.OnClickListener {
+    static final int REQUEST_IMAGE_GET = 1;
+    private static final String TAG = "NewStudyCaseActivity";
+
     private FirebaseDatabase database;
     private DatabaseReference studycasesReference;
+
+    StorageReference storageRef;
+
+    Button buttoncreate;
+    Button buttonchoose;
+
+    TextInputEditText name;
+    TextInputEditText desc;
+
+    String idExam;
 
     @Override
     protected int getLayoutId() {
@@ -36,35 +63,26 @@ public class NewStudyCaseActivity extends AbstractFormActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        Button button = findViewById(R.id.create_study_case);
+        buttoncreate = findViewById(R.id.button_create_study_case);
+        buttonchoose = findViewById(R.id.button_select_file);
 
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                TextInputEditText name = (TextInputEditText) findViewById(R.id.name_edit_text);
-                TextInputEditText desc = (TextInputEditText) findViewById(R.id.desc_edit_text);
+        buttoncreate.setOnClickListener(this);
+        buttonchoose.setOnClickListener(this);
 
-                if(validate(name,desc)) {
+        name = (TextInputEditText) findViewById(R.id.name_edit_text);
+        desc = (TextInputEditText) findViewById(R.id.desc_edit_text);
 
-                    database = FirebaseDbHelper.getDBInstance();
-                    studycasesReference = database.getReference(FirebaseDbHelper.TABLE_STUDYCASES);
+        database=FirebaseDbHelper.getDBInstance();
+        studycasesReference=database.getReference(FirebaseDbHelper.TABLE_STUDYCASES);
 
-                    //Ho modificato questa parte per inizializzare il caso di studio con un id.
-                    DatabaseReference newElement = studycasesReference.push();
+        Intent intent=getIntent();
+        idExam = intent.getStringExtra(Exam.Keys.ID);
 
-                    Intent intent = getIntent();
+        storageRef = FirebaseStorage.getInstance().getReference();
+        Log.d(TAG, "oncreate");
 
-                    StudyCase studycase = new StudyCase(newElement.getKey(),
-                            name.getText().toString(), desc.getText().toString(),intent.getStringExtra(Exam.Keys.ID));
-                    newElement.setValue(studycase);
-
-                    Toast.makeText(getApplicationContext(), R.string.text_message_study_case_created, Toast.LENGTH_SHORT).show();
-                    NewStudyCaseActivity.super.onBackPressed();
-                }
-
-            }
-        });
     }
+
 
    private boolean validate(EditText name, EditText desc){
         boolean valid = true;
@@ -80,6 +98,91 @@ public class NewStudyCaseActivity extends AbstractFormActivity {
         }
 
         return valid;
+    }
+
+    @Override
+    public void onClick(View v) {
+        Log.d(TAG, "Switch");
+        switch (v.getId()) {
+            case R.id.button_select_file:
+                selectDocument();
+                Log.d(TAG, "dentro case Switch");
+                break;
+            case R.id.button_create_study_case:
+              // createNewStudyCase();
+                break;
+        }
+    }
+
+    private void createNewStudyCase(Uri file){
+        if(validate(name,desc)){
+            Log.d(TAG, "createstudycase");
+            //Ho modificato questa parte per inizializzare il caso di studio con un id.
+            DatabaseReference newElement=studycasesReference.push();
+
+            StudyCase studycase=new StudyCase(newElement.getKey(),
+            name.getText().toString(),desc.getText().toString(),idExam);
+            newElement.setValue(studycase);
+
+            // Create the file metadata
+            StorageMetadata metadata = new StorageMetadata.Builder()
+                    .setContentType(FileUtil.getMimeTypeFromUri(NewStudyCaseActivity.this, file))
+                    .build();
+
+            // Upload file and metadata to the path 'images/mountains.jpg'
+            UploadTask uploadTask = storageRef.child("Exam"+idExam).child("StudyCase"+newElement.getKey())
+                    .child(FileUtil.getFileNameFromURI(NewStudyCaseActivity.this, file))
+                    .putFile(file, metadata);
+
+            // Listen for state changes, errors, and completion of the upload.
+            uploadTask.addOnProgressListener(new OnProgressListener<UploadTask.TaskSnapshot>() {
+                @Override
+                public void onProgress(@NonNull UploadTask.TaskSnapshot taskSnapshot) {
+                    // TODO implementare schermata di dialogo con progresso
+                    double progress = (100.0 * taskSnapshot.getBytesTransferred()) / taskSnapshot.getTotalByteCount();
+                    Log.d(TAG, "Upload is " + progress + "% done");
+                }
+            }).addOnPausedListener(new OnPausedListener<UploadTask.TaskSnapshot>() {
+                @Override
+                public void onPaused(@NonNull UploadTask.TaskSnapshot taskSnapshot) {
+                    Log.d(TAG, "Upload is paused");
+                }
+            }).addOnFailureListener(new OnFailureListener() {
+                @Override
+                public void onFailure(@NonNull Exception exception) {
+                    // TODO Handle unsuccessful uploads
+                }
+            }).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                @Override
+                public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                    Toast.makeText(getApplicationContext(),R.string.text_message_study_case_created,Toast.LENGTH_SHORT).show();
+                    NewStudyCaseActivity.super.onBackPressed();
+                    // TODO Handle successful uploads on complete
+                    // ...
+                }
+            });
+
+        }
+    }
+
+    private void selectDocument() {
+        Log.d(TAG, "Selectdocument");
+        Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
+        intent.setType("*/*");
+        startActivityForResult(intent, REQUEST_IMAGE_GET);
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == REQUEST_IMAGE_GET && resultCode == RESULT_OK) {
+            Bitmap thumbnail = data.getParcelableExtra("data"); /*TODO farci qualcosa come mostrare una finestra di dialogo
+                                                                         che mostri il progresso dell'upload e che usi il thumbnail*/
+            Uri fullFileUri = data.getData();
+            Log.d(TAG, "dentro if onactivityresult");
+            createNewStudyCase(fullFileUri);
+        }
     }
 
 }
